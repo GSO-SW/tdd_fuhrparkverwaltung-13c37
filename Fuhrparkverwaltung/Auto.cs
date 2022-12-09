79fb7e50fe0be40382903fb0e4702ee3095f323e
Berkant <dernamentyp@protonmail.com>
2022-12-09 09:12:19 +0100

    Autoklasse implementiert für Fahren_SteigertKilometerstand.

---
+++ Fuhrparkverwaltung/Auto.cs
@@ -0,0 +1,25 @@
+﻿namespace Fuhrparkverwaltung
+{
+    public class Auto
+    {
+        private int kilometerstand;
+
+        public Auto(int kilometerstand)
+        {
+            this.kilometerstand = kilometerstand;
+        }
+
+        public void Fahren(int streckeInKilometern)
+        {
+            kilometerstand = kilometerstand + streckeInKilometern;
+        }
+
+        public int Kilometerstand
+        {
+            get
+            {
+                return kilometerstand;
+            }
+        }
+    }
+}
